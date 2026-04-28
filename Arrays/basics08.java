b88c5e6a4178a306b7c6830ed12cdeb37b9907ef
satwik mishra <satwikmishra074@gmail.com>
2026-04-28 18:58:10 +0530

    swap alternate elements

--- Arrays/basics08.java
+++ Arrays/basics08.java
@@ -2,12 +2,23 @@ package Arrays;
 // swap alternate elements in an array 
 // {1,2,3,4,5,6} --> {2,1,4,3,6,5}
 public class basics08 {
-    public static int[] swapped(int arr[])
-    {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5};
         int i=0;
-        for(int j=0;j<i+2;j+=2)
+        int j=i+1;
+        int size=arr.length;
+        while(i<size && j<size)
         {
-            int temp=
+            int temp=arr[j];
+            arr[j]=arr[i];
+            arr[i]=temp;
+            i=j+1;
+            j=i+1;
+        }
+        
+        for(int k=0;k<size;k++)
+        {
+            System.out.println(arr[k]);
         }
     }
 }
